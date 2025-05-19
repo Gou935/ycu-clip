684030ffb3106b75ad16fb4eef88752ec2e5b501
Gou935 <103366099+Gou935@users.noreply.github.com>
2025-05-19 16:15:28 +0900

    push.sh changed

--- push.sh
+++ push.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-git add .
+# unstaged changes check
+if ! git diff --quiet; then
+  echo "Unstaged changes detected.(git add or git stash)."
+  git status
+  exit 1
+fi
 
-if git diff-index --quiet HEAD; then
-  echo -e "\e[93m沒有變更可提交。\e[0m"
+# Check for staged but uncommitted changes
+if ! git diff --cached --quiet; then
+  echo "Found staged changes."
+else
+  echo "No staged changes yet. Staging all changes with 'git add .'"
+  git add .
+fi
+
+# git pull --rebase
+echo "git pull --rebase"
+if ! git pull origin main --rebase; then
+  echo  "pull failed, check your commit status."
+  exit 1
+fi
+echo  "successfully pulled from main branch"
+
+# commit and push
+if git diff-index --quiet HEAD --; then
+  echo  "no changes to commit"
 else
   git commit -m "website update：$(date '+%Y-%m-%d %H:%M')"
-  git push
-  echo -e "\e[32m✅ Deployed to GitHub Pages.\e[0m"
+  git push origin main
+  echo  "✅ Deployed to GitHub Pages"
 fi
+
